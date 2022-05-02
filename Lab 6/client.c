7367b556d42f81a404f218b9829fff81b79c8385
Alexantoun <oz.antoun@gmail.com>
2022-05-01 18:48:06 -0600

    Made two programs talk to each other

--- Lab 6/client.c
+++ Lab 6/client.c
@@ -1,3 +1,6 @@
+//So you first need to telnet to the IP address and correct port number for the client to work 
+//This program just makes a connection to a server and sends something 
+
 #include <stdio.h>
 #include <sys/socket.h> //This allows us to make a socket
 #include <arpa/inet.h>  //This lets us specify IP addresses
@@ -7,12 +10,31 @@
 #include <getopt.h>     //gives us command line arguments much easier
 
 int main(int argc, char **argv){
-    struct sockaddr_in serv_addr;
-    serv_addr.sin_port = htons(4321);
-    serv_addr.sin_addr.s_addr = inet_addr("192.168.1.6");
-    printf("IP Address is 192.168.1.6\n");
+    struct sockaddr_in serv_addr;       //Not defining a struct, in C when you use a struct, you must specifiy that it is a struct.
+    serv_addr.sin_family = AF_INET;     //Need to give the family type of our sockaddr
+    serv_addr.sin_port = htons(4321);  //We specify the port we want to connect to --> networks use big endian --> htons = host to networks 
+                                        //So htons is converting the number to big endian
+    serv_addr.sin_addr.s_addr = inet_addr("192.168.1.6"); // the ip address that you want to connect to
+
+    char buffer[1024] = "\0";
+
+    int sock;   //socket function below returns a socket
+                //Socket function is int socket(int domain, int type, int protocol), specify 0 for protocol 
+                //to mean "any" from <sys/socket>
+
+    sock = socket(AF_INET, SOCK_STREAM, 0);     // creates a socket
     
+    //int connect(int sockfd, struct sockaddr, int addrlen)
+    connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)); 
 
+    //send you give it a socket, a mesasge, the length of message, then finally some options or 0 for no options
+    //printf("Our string is: %s\n", name);
+    send(sock, argv, strlen(argv), 0);
+    recv(sock, buffer, 1024,0 );  //Recv recieves a message from a socket(sock) stores it into a buffer (buffer), 
+                                //then len is the length of the recieve signal the zero is for no flags
 
+    //Instead of recv() try this --> read(sock,buffer, 16);
+    printf("%s\n",buffer);                    
+ 
     return 0;
 }
--- Lab 6/server.c
+++ Lab 6/server.c
@@ -10,6 +10,47 @@
 #include <getopt.h>
 #include <arpa/inet.h>
 
+
+const static char* returnEcho = "This is the returned message\n";
+const static char* invalidOption = "That wasn't very cash money of you\n";
+
 int main(int argc, char **argv){
+    struct addrinfo hints, *results; 
+    struct sockaddr_storage remoteAddress;
+
+    int serverSocket;
+
+    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    memset (&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;    //IPv4 or IPv6...Doesnt matter
+    hints.ai_socktype = SOCK_STREAM;    //TCP stream sockets
+    hints.ai_flags = AI_PASSIVE;        //Fills in IP for me
+
+    getaddrinfo(NULL, "4321", &hints, &results);    //Fills in the struct information for us
+    bind(serverSocket, results->ai_addr, results->ai_addrlen);  //Associate the socket with a specific port
+    listen(serverSocket, 1);    //listen on a socket and will accept 1 connection
+    int addrlen = sizeof(struct sockaddr_storage);
+    int incomingSocket = accept(serverSocket, (struct sockaddr*)&remoteAddress, &addrlen);
     
+    char mode [103]; //100 Characters + Carriage return + LineFeed + nullterminator
+    memset(mode, '\0', 103);
+    int recv_len = -1;
+    
+    while(recv_len != 0){
+        recv_len = recv(incomingSocket, &mode, 102, 0); //recv_len == 0 iff client disconnects
+        printf("Recieved %c\n", mode[0]);
+        if (mode[0] == '1'){
+            send(incomingSocket, returnEcho, strlen(returnEcho), 0);
+        }
+        else if (mode[0] == '2'){
+             send(incomingSocket, "Selected file transfer\n", strlen("Selected file transfer\n"), 0);
+        }
+        else{
+            send(incomingSocket, "You sent: ", strlen("You sent: "), 0);
+            send(incomingSocket, &mode, strlen(mode), 0);
+        }
+        memset(mode, '\0', 103);
+    }
+    printf("Goodbye\n");
+
 }
