0fac0b45c0eb173f9e8f27940c2fb8e80863a674
Alexantoun <oz.antoun@gmail.com>
2022-05-01 19:32:06 -0600

    Fixed a bug I found last second

--- Lab 6/client.c
+++ Lab 6/client.c
@@ -15,7 +15,6 @@ int main(int argc, char **argv){
     serv_addr.sin_port = htons(4321);  //We specify the port we want to connect to --> networks use big endian --> htons = host to networks 
                                         //So htons is converting the number to big endian
     serv_addr.sin_addr.s_addr = inet_addr("192.168.1.6"); // the ip address that you want to connect to
-
     char buffer[1024] = "\0";
 
     int sock;   //socket function below returns a socket
@@ -29,8 +28,8 @@ int main(int argc, char **argv){
 
     //send you give it a socket, a mesasge, the length of message, then finally some options or 0 for no options
     //printf("Our string is: %s\n", name);
-    send(sock, argv, strlen(argv), 0);
-    recv(sock, buffer, 1024,0 );  //Recv recieves a message from a socket(sock) stores it into a buffer (buffer), 
+    send(sock, argv[1], strlen(argv[1]), 0);
+    recv(sock, buffer, 1024, 0);  //Recv recieves a message from a socket(sock) stores it into a buffer (buffer), 
                                 //then len is the length of the recieve signal the zero is for no flags
 
     //Instead of recv() try this --> read(sock,buffer, 16);
--- Lab 6/server.c
+++ Lab 6/server.c
@@ -28,22 +28,25 @@ int main(int argc, char **argv){
 
     getaddrinfo(NULL, "4321", &hints, &results);    //Fills in the struct information for us
     bind(serverSocket, results->ai_addr, results->ai_addrlen);  //Associate the socket with a specific port
-    listen(serverSocket, 1);    //listen on a socket and will accept 1 connection
-    int addrlen = sizeof(struct sockaddr_storage);
-    int incomingSocket = accept(serverSocket, (struct sockaddr*)&remoteAddress, &addrlen);
+    
     
     char mode [103]; //100 Characters + Carriage return + LineFeed + nullterminator
     memset(mode, '\0', 103);
     int recv_len = -1;
     
-    while(recv_len != 0){
+    while(1){
+        listen(serverSocket, 1);    //listen on a socket and will accept 1 connection
+        int addrlen = sizeof(struct sockaddr_storage);
+        int incomingSocket = accept(serverSocket, (struct sockaddr*)&remoteAddress, &addrlen);
         recv_len = recv(incomingSocket, &mode, 102, 0); //recv_len == 0 iff client disconnects
-        printf("Recieved %c\n", mode[0]);
         if (mode[0] == '1'){
             send(incomingSocket, returnEcho, strlen(returnEcho), 0);
         }
         else if (mode[0] == '2'){
              send(incomingSocket, "Selected file transfer\n", strlen("Selected file transfer\n"), 0);
+        }
+        else if(mode == "Goodbye\r\n"){
+            recv_len = 0;
         }
         else{
             send(incomingSocket, "You sent: ", strlen("You sent: "), 0);
