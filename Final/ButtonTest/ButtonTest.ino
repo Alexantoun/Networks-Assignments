c0d7f750e687fe83ab6891d745b8125f41dd3c3a
Alexantoun <oz.antoun@gmail.com>
2022-05-06 13:10:41 -0600

    Can now work with Temperature and humidity sensors

--- Final/ButtonTest/ButtonTest.ino
+++ Final/ButtonTest/ButtonTest.ino
@@ -1,19 +1,21 @@
 #include <ESP8266WiFi.h>
-#include <dht.h>
-
-#define inRead A0
-dht DHT;
-/*const char* ssid = "OceanFast_2GEXT";
-const char* psswrd = "IDontKnow1";
-const char* host ="192.168.1.24";
-const uint16_t port = 2345;*/
+#include "DHT.h"
+#define inRead 12
+DHT dht(inRead, DHT11);
+const char* ssid = "Alex's iPhone (2)";
+const char* psswrd = "alex1234";
+const char* host ="172.20.10.9";
+const uint16_t port = 2345;
 int bttnState = 0;
+float temp;
+float humid;
+int status;
                     
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  /*Serial.print("Connecting to ");
+  Serial.print("Connecting to ");
   Serial.println(ssid);
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, psswrd);
@@ -25,9 +27,10 @@ void setup() {
   
   Serial.print("WiFi Connected! Local IP is: ");
   Serial.print(WiFi.localIP());
-  Serial.println("");*/
+  Serial.println("");
   pinMode(D3, INPUT);
-  pinMode(A0, INPUT);
+  pinMode(inRead, INPUT);
+  dht.begin();
   pinMode(D4, OUTPUT);
   digitalWrite(D4, HIGH);
 }
@@ -35,12 +38,12 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
- /* WiFiClient client;
+  WiFiClient client;
   if(!client.connect(host,port)){
     Serial.println("Connection Failed");
     delay(5000);
   }
-  //while(client.connected()){
+  while(client.connected()){
     bttnState = digitalRead(D3);
     if(bttnState == LOW){
       digitalWrite(D4, HIGH);
@@ -49,14 +52,17 @@ void loop() {
     else{
       digitalWrite(D4, LOW);
       client.print("Button not being pressed");
-    }  */
-    DHT.read11(inRead);
+    }  
+    status = dht.read(inRead);
+    Serial.println(status);
+    temp = dht.readTemperature();
+    Serial.println(temp);
     Serial.print("Tempreture read = ");
-    Serial.print(DHT.temperature);
-    Serial.print("Humid Read = ");
-    Serial.print(DHT.humidity);
-    delay(500);
-  //}
+    Serial.print(temp);
+    client.print("Humid Read = ");
+    client.print(dht.readHumidity());
+    delay(2000);
+  }
 
   Serial.println("Disconnected from server");
   delay(500000);
